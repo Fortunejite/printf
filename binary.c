9e5708280541e43d61fc7d5cbe13912e97f25572
OnajiteChristabel <christabelonajite2005@gmail.com>
2022-10-17 08:19:36 -0700

    nfhnfucnxl

--- binary.c
+++ binary.c
@@ -15,12 +15,10 @@ char *itob(va_list list)
 	k = va_arg(list, int);
 	i = k;
 
-	/* malloc up to max int in binary */
 	s = malloc(sizeof(char) * 33);
 	if (s == NULL)
 		return (NULL);
 
-	/* account for negative numbers with '1' at index 0 */
 	if (k < 0)
 	{
 		s[0] = 1 + '0';
@@ -29,14 +27,12 @@ char *itob(va_list list)
 		i *= -1;
 	}
 
-	/* find biggest power of 2 it's divisible by */
 	while (k > 1)
 	{
 		k /= 2;
 		twos *= 2;
 	}
 
-	/* divide down and store binary num */
 	while (twos > 0)
 	{
 		s[j++] = (i / twos + '0');
